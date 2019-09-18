2c465e39176f613e283fc8982d30f47e78f82997
toshiki_sakamoto <dearprograms2015@gmail.com>
2019-09-18 09:24:07 +0900

    k

---
+++ Assets/App/Scripts/Common/Chara/AnimationStateController.cs
@@ -0,0 +1,82 @@
+﻿// 
+// AnimationStateController.cs  
+// ProductName Ling
+//  
+// Create by toshiki sakamoto on 2019.09.18.
+// 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Ling.Common.Chara
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AnimationStateController : MonoBehaviour 
+    {
+        #region 定数, class, enum
+
+        #endregion
+
+
+        #region public 変数
+
+        #endregion
+
+
+        #region private 変数
+
+        #endregion
+
+
+        #region プロパティ
+
+        #endregion
+
+
+        #region public, protected 関数
+
+        #endregion
+
+
+        #region private 関数
+
+        #endregion
+
+
+        #region MonoBegaviour
+
+        /// <summary>
+        /// 初期処理
+        /// </summary>
+        void Awake()
+        {
+        }
+
+        /// <summary>
+        /// 更新前処理
+        /// </summary>
+        void Start()
+        {
+        }
+
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        void Update()
+        {
+        }
+
+        /// <summary>
+        /// 終了処理
+        /// </summary>
+        void OnDestoroy()
+        {
+        }
+
+        #endregion
+    }
+}
