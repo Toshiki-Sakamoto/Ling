4ec3ab302b44e121e8396afb0041642dfd68be9d
Toshiki Sakamoto <dearprograms2015@gmail.com>
2019-10-12 15:42:00 +0900

    一旦込っち

---
+++ Assets/App/Scripts/Utility/TileEvent/TileEvent.cs
@@ -0,0 +1,73 @@
+﻿//
+// TileEvent.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2019.10.04
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+
+namespace Ling.Utility.TileEvent
+{
+	/// <summary>
+	/// 
+	/// </summary>
+    [System.Serializable]
+    public class TileEvent
+    {
+        #region 定数, class, enum
+
+        #endregion
+
+
+        #region public, protected 変数
+
+        #endregion
+
+
+        #region private 変数
+
+        [SerializeField] private string _name;  // タイル名
+        [SerializeField] private EventTriggerType _trigger;
+        [SerializeField] private UnityEvent _onEvent;
+        
+
+        private TileBase _tile = null;
+        private int _posX;
+        private int _posY;
+        private float _worldX;
+        private float _worldY;
+
+        #endregion
+
+
+        #region プロパティ
+
+        #endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+        #region public, protected 関数
+
+        #endregion
+
+
+        #region private 関数
+
+        #endregion
+    }
+}
---
+++ Assets/App/Scripts/Utility/TileEvent/TileEvents.cs
@@ -0,0 +1,127 @@
+﻿// 
+// TileEvents.cs  
+// ProductName Ling
+//  
+// Create by toshiki sakamoto on 2019.10.04.
+// 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+
+namespace Ling.Utility.TileEvent
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [RequireComponent(typeof(Tilemap))]
+    [RequireComponent(typeof(TilemapRenderer))]
+    [RequireComponent(typeof(TilemapCollider2D))]
+    public class TileEvents : MonoBehaviour
+    {
+        #region 定数, class, enum
+
+        #endregion
+
+
+        #region public 変数
+
+        #endregion
+
+
+        #region private 変数
+
+        private bool _isInitialized;
+        private Tilemap _eventsMap;
+        private BoundsInt _lastMapBounds;
+        private TilemapRenderer _eventsRenderer;
+        private TilemapCollider2D _eventsCollider;
+
+        //private List<EventTile> _tiles = new List<EventTile>();
+
+        #endregion
+
+
+        #region プロパティ
+
+        #endregion
+
+
+        #region public, protected 関数
+
+        #endregion
+
+
+        #region private 関数
+
+        private void Init(bool isEditor = false)
+        {
+            if (_isInitialized)
+            {
+                return;
+            }
+
+            _eventsMap = GetComponent<Tilemap>();
+            _lastMapBounds = _eventsMap.cellBounds;
+            _eventsRenderer = GetComponent<TilemapRenderer>();
+
+            // エディターじゃない場合は隠す
+            if (!isEditor)
+            {
+                _eventsRenderer.sortingLayerName = "Default";
+                _eventsRenderer.sortingOrder = -9999;
+            }
+
+            _eventsCollider = GetComponent<TilemapCollider2D>();
+
+            // 
+
+            _eventsCollider.isTrigger = true;
+
+            /*
+            if (Tiles == null)
+            {
+
+            }*/
+
+            _isInitialized = true;
+        }
+
+        #endregion
+
+
+        #region MonoBegaviour
+
+        /// <summary>
+        /// 初期処理
+        /// </summary>
+        void Awake()
+        {
+        }
+
+        /// <summary>
+        /// 更新前処理
+        /// </summary>
+        void Start()
+        {
+        }
+
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        void Update()
+        {
+        }
+
+        /// <summary>
+        /// 終了処理
+        /// </summary>
+        void OnDestoroy()
+        {
+        }
+
+        #endregion
+    }
+}
