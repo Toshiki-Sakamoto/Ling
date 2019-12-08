538d427692eb9066e80127019b402d00584f394b
toshiki_sakamoto <dearprograms2015@gmail.com>
2019-12-08 11:34:35 +0900

    map作成テスト

---
+++ Assets/App/Scripts/Map/Builder.cs
@@ -0,0 +1,60 @@
+﻿//
+// Builder.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2019.12.08
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Ling.Map
+{
+	/// <summary>
+	/// 
+	/// </summary>
+    public abstract class Builder
+    {
+        #region 定数, class, enum
+
+        #endregion
+
+
+        #region public, protected 変数
+
+        #endregion
+
+
+        #region private 変数
+
+        #endregion
+
+
+        #region プロパティ
+
+        #endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+        #region public, protected 関数
+
+//        public abstract void Create
+
+        #endregion
+
+
+        #region private 関数
+
+        #endregion
+    }
+}
