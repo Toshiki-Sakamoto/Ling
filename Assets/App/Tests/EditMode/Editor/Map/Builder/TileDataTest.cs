88ed804ce1c1ad92ecd9a2f164771eb01884dc16
toshiki_sakamoto <dearprograms2015@gmail.com>
2019-12-30 16:30:12 +0900

    TileDataTest追加

---
+++ Assets/App/Tests/EditMode/Editor/Map/Builder/TileDataTest.cs
@@ -0,0 +1,77 @@
+﻿//
+// TileDataTest.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2019.12.30
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+using NUnit.Framework;
+
+using Assert = UnityEngine.Assertions.Assert;
+
+using Ling.Map.Builder;
+
+
+namespace Ling.Tests.Map.Builder
+{
+	/// <summary>
+	/// TileData のテスト
+	/// </summary>
+	public class TileDataTest
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		private TileData _tileData;
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+
+		[Test]
+		public void TileData_HasFlag()
+		{
+
+		}
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
