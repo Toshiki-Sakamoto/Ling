cd222cfc9464855fed721798b1658764f63d4436
toshiki_sakamoto <dearprograms2015@gmail.com>
2020-04-30 19:38:36 +0900

    Templateスクリプト変更 / Zenjectを入れた

--- Assets/App/Scripts/Common/Editor/CustomScript/MonoCreator.cs
+++ Assets/App/Scripts/Common/Editor/CustomScript/MonoCreator.cs
@@ -10,7 +10,7 @@ namespace Ling.Common.Editor.CustomScript
     {
         private const string TEMPLATE_SCRIPT_NAME = "MonoClass";
 
-        [MenuItem(Const.MENU_PATH + TEMPLATE_SCRIPT_NAME)]
+        [MenuItem(TEMPLATE_SCRIPT_NAME, menuItem = Const.MENU_PATH + TEMPLATE_SCRIPT_NAME, priority = 1)]
         private static void CreateScript()
         {
             ShowWindow(TEMPLATE_SCRIPT_NAME);
