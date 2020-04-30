fa85323546dda8a6392bea130f020d364a99d467
toshiki_sakamoto <dearprograms2015@gmail.com>
2020-04-30 15:42:33 +0900

    ランダム処理を別クラスに分けた

---
+++ Assets/App/Scripts/Utility/Random.cs
@@ -0,0 +1,38 @@
+﻿//
+// Random.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2020.04.30
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Ling.Utility
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class Random
+    {
+		/// <summary>
+		/// Maxも範囲に含める
+		/// </summary>
+		public static int Range(int min, int max) => UnityEngine.Random.Range(min, max + 1);
+		public static float Range(float min, float max) => UnityEngine.Random.Range(min, max + 1);
+
+		/// <summary>
+		/// 0からMaxまでのランダム値を取る
+		/// </summary>
+		/// <param name="max"></param>
+		/// <returns></returns>
+		public static int Range(int max) => Range(0, max);
+		public static float Range(float max) => Range(0, max);
+	}
+}
