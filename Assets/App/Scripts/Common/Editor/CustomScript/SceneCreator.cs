afe348b406dde9cc73637b4e1df72382a1ec0815
toshiki_sakamoto <dearprograms2015@gmail.com>
2020-04-30 21:00:59 +0900

    Sceneテンプレートスクリプトを作成できるようにした

---
+++ Assets/App/Scripts/Common/Editor/CustomScript/SceneCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+namespace Ling.Common.Editor.CustomScript
+{
+	public class SceneCreator : Creator
+	{
+		private const string TEMPLATE_SCRIPT_NAME = "SceneClass";
+
+		[MenuItem(TEMPLATE_SCRIPT_NAME, menuItem = Const.MENU_PATH + TEMPLATE_SCRIPT_NAME, priority = 1)]
+		private static void CreateScript()
+		{
+			ShowWindow(TEMPLATE_SCRIPT_NAME);
+		}
+	}
+}
