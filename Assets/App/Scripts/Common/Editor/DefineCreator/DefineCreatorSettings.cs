26975e47201a2ad2cc819737424f1e3548ae2bc8
toshiki_sakamoto <dearprograms2015@gmail.com>
2020-07-19 14:43:01 +0900

    フォルダとファイル整理

---
+++ Assets/App/Scripts/Common/Editor/DefineCreator/DefineCreatorSettings.cs
@@ -0,0 +1,62 @@
+﻿//
+// DefineCreatorSettings.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2020.07.19
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Zenject;
+
+namespace Ling.Common.Editor.DefineCreator
+{
+	/// <summary>
+	/// DefineCreator内の設定
+	/// </summary>
+	[CreateAssetMenu(menuName = "Ling/DefineCreator/Settings")]
+	public class DefineCreatorSettings : ScriptableObject
+    {
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		public string saveDirectoryPath;	// 保存先ディレクトリパス
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
---
+++ Assets/App/Scripts/Common/Editor/Utility/TouchPointEventTriggerEditor.cs
@@ -0,0 +1,66 @@
+﻿// 
+// TouchPointEventTriggerEditor.cs  
+// ProductName Ling
+//  
+// Created by toshiki sakamoto on 2020.04.28
+// 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEditor.EventSystems;
+using Ling.Utility;
+ 
+
+
+namespace Ling.Editor.Utility
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[CustomEditor(typeof(TouchPointEventTrigger))]
+	public class TouchPointEventTriggerEditor : EventTriggerEditor 
+    {
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			base.OnInspectorGUI();
+		}
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+
+
+		#region MonoBegaviour
+
+		#endregion
+	}
+}
