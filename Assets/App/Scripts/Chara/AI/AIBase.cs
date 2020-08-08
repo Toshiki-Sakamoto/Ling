653e5806be72f6669d343772f58a9661563f76ea
toshiki_sakamoto <dearprograms2015@gmail.com>
2020-08-08 16:12:04 +0900

    キャラクタのAIクラス作成

---
+++ Assets/App/Scripts/Chara/AI/AIBase.cs
@@ -0,0 +1,54 @@
+﻿//
+// AIBase.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2020.07.26
+//
+
+namespace Ling.Chara.AI
+{
+	/// <summary>
+	/// AIのベースクラス
+	/// </summary>
+	public abstract class AIBase
+    {
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		/// <summary>
+		/// AIの種類
+		/// </summary>
+		public abstract Ling.Const.MoveAIType AIType { get; }
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
