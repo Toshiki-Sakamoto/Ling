3c4921225f41b014dafad712c8cfdaa6478dcc2e
坂本寿樹 <dearprograms2015@gmail.com>
2020-08-09 19:02:58 +0900

    Mapにフラグを更新するイベント、Charaに自分の座標が変更されたときに発行されるイベントを追加

---
+++ Assets/App/Scripts/Chara/CharaEvents.cs
@@ -0,0 +1,21 @@
+﻿//
+// CharaEvents.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2020.08.09
+//
+
+using UnityEngine;
+
+namespace Ling.Chara
+{
+	/// <summary>
+	/// 移動した
+	/// </summary>
+	public class EventPosUpdate
+	{
+		public Vector2Int prevPos;	// 以前の座標
+		public Vector3Int newPos;	// 今回の座標
+		public CharaType charaType;
+	}
+}
