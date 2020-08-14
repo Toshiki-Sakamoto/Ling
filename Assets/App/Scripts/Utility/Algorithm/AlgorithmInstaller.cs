876996abca363a57ba1fb034ac81c35cd9d0ac08
坂本寿樹 <dearprograms2015@gmail.com>
2020-08-14 16:03:03 +0900

    UtilityフォルダにAlgorithmをまとめるためのフォルダ追加。AstarAlgorithmクラス作詞絵

---
+++ Assets/App/Scripts/Utility/Algorithm/AlgorithmInstaller.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Zenject;
+
+namespace Ling.Utility.Algorithm
+{
+    public class AlgorithmInstaller : MonoInstaller
+    {    public override void InstallBindings()
+        {
+                Container
+                    .Bind<Search>()
+                    .FromComponentInHierarchy()
+                    .AsSingle();
+        }
+    }
+}
---
+++ Assets/App/Scripts/Utility/Algorithm/Astar.cs
@@ -0,0 +1,56 @@
+﻿//
+// Astar.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2020.08.14
+//
+
+using UnityEngine;
+
+namespace Ling.Utility.Algorithm
+{
+	/// <summary>
+	/// AStartAlgorithm
+	/// </summary>
+	public class Astar
+    {
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		public void Setup()
+		{
+			
+		}
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
