015d1dc2569076a42b427c78069c522505c3b32b
坂本寿樹 <dearprograms2015@gmail.com>
2020-09-12 19:21:06 +0900

    CommonにカスタムReactivePropertyを追加していけるようにフォルダ作成。 VectorIntXX 系を追加

---
+++ Assets/App/Scripts/Common/ReactiveProperty/VectorIntReactiveProperty.cs
@@ -0,0 +1,37 @@
+﻿//
+// VectorIntReactiveProperty.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2020.09.12
+//
+
+using UniRx;
+using UnityEngine;
+
+namespace Ling.Common.ReactiveProperty
+{
+	/// <summary>
+	/// VectorIntXX ReactiveProperty
+	/// 追加した場合 Editor.ExtendInspectorDisplayDrawerにも追加していくこと
+	/// </summary>
+
+	[System.Serializable]
+	public class Vector2IntReactiveProperty : ReactiveProperty<Vector2Int>
+	{
+		public Vector2IntReactiveProperty() {}
+
+		public Vector2IntReactiveProperty(Vector2Int initialValue)
+			:base(initialValue)
+		{}
+	}
+
+	[System.Serializable]
+	public class Vector3IntReactiveProperty : ReactiveProperty<Vector3Int>
+	{
+		public Vector3IntReactiveProperty() {}
+
+		public Vector3IntReactiveProperty(Vector3Int initialValue)
+			:base(initialValue)
+		{}
+	}
+}
