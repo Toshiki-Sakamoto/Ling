ac24a71abd7f2df38a09c1a4f4dc79f0fa60c844
坂本寿樹 <dearprograms2015@gmail.com>
2020-09-20 01:27:41 +0900

    UniRxのメッセージ関連のテストクラス作成 OnNextに値の意味はないが通知を送るUnitのテスト追加

---
+++ Assets/App/Tests/PlayMode/Plugin/UniRx/UniRxMessageTest.cs
@@ -0,0 +1,73 @@
+﻿//
+// UniRxMessageTest.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2020.09.20
+//
+
+using NUnit.Framework;
+using UniRx;
+using System;
+
+using Assert = UnityEngine.Assertions.Assert;
+
+
+namespace Ling.Tests.PlayMode.Plugin.UniRx
+{
+	/// <summary>
+	/// UniRxのメッセージ関するテスト
+	/// </summary>
+	public class UniRxMessageTest
+    {
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		/// <summary>
+		/// 意味のない通知を送るテスト
+		/// </summary>
+		[Test]
+		public void MessageUnitDefaultTest()
+		{
+			int count = 0;
+
+			var subject = new Subject<Unit>();
+			subject.Subscribe(_ =>  count++);
+
+			// Unit型はそれ自身は特に意味はない。通知を贈りたいときに利用
+			subject.OnNext(Unit.Default);
+
+			Assert.AreEqual(1, count, "通知が届いているので1");
+		}
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
