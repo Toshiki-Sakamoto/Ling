198cce89795c03a3cd0a25fec4a97809e812ebb2
坂本寿樹 <dearprograms2015@gmail.com>
2020-11-02 22:37:31 +0900

    StatusScene&View追加

---
+++ Assets/App/Scripts/Scenes/Status/StatusScene.cs
@@ -0,0 +1,57 @@
+﻿// 
+// StatusScene.cs  
+// ProductName Ling
+//  
+// Created by toshiki sakamoto on 2020.11.02
+// 
+
+using UnityEngine;
+
+namespace Ling.Scenes.Status
+{
+	/// <summary>
+	/// Status UI Scene
+	/// </summary>
+	/// <remark>
+	/// プレイヤーのHPやアイテムなどのUI
+	/// </reamsk>
+	public class StatusScene : Common.Scene.Base  
+    {
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		[SerializeField] private StatusView _view = default;
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+
+
+		#region MonoBegaviour
+
+
+		#endregion
+	}
+}
---
+++ Assets/App/Scripts/Scenes/Status/StatusView.cs
@@ -0,0 +1,54 @@
+﻿//
+// StatusView.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2020.11.02
+//
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ling.Scenes.Status
+{
+	/// <summary>
+	/// Status View
+	/// </summary>
+	public class StatusView : MonoBehaviour
+    {
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		[SerializeField] private Text _hpText = default;
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
