acb2e8a283f50c43c0443dfd994a96a4cd4ad47f
坂本寿樹 <dearprograms2015@gmail.com>
2020-11-07 23:37:23 +0900

    アイテムのデータクラスとViewクラスの作成

---
+++ Assets/App/Scripts/Item/ItemView.cs
@@ -0,0 +1,79 @@
+﻿// 
+// ItemView.cs  
+// ProductName Ling
+//  
+// Created by toshiki sakamoto on 2020.11.07
+// 
+
+using UnityEngine;
+
+namespace Ling.Item
+{
+	/// <summary>
+	/// アイテムの見た目を管理
+	/// </summary>
+	public class ItemView : MonoBehaviour 
+    {
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+
+
+		#region MonoBegaviour
+
+		/// <summary>
+		/// 初期処理
+		/// </summary>
+		void Awake()
+		{
+		}
+
+		/// <summary>
+		/// 更新前処理
+		/// </summary>
+		void Start()
+		{
+		}
+
+		/// <summary>
+		/// 更新処理
+		/// </summary>
+		void Update()
+		{
+		}
+
+		/// <summary>
+		/// 終了処理
+		/// </summary>
+		void OnDestoroy()
+		{
+		}
+
+		#endregion
+	}
+}
