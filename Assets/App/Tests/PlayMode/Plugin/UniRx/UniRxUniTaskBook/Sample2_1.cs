4b8fff87c3f6b2433af95b7ffae23855cfe4873b
坂本寿樹 <dearprograms2015@gmail.com>
2020-11-22 17:16:47 +0900

    UniRx/UniTask完全理解 本サンプル

---
+++ Assets/App/Tests/PlayMode/Plugin/UniRx/UniRxUniTaskBook/Sample2_1.cs
@@ -0,0 +1,102 @@
+﻿//
+// Test2.1.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2020.11.22
+//
+
+using NUnit.Framework;
+using UniRx;
+using System;
+using UnityEngine.TestTools;
+using UnityEngine;
+using System.Collections;
+
+using Assert = UnityEngine.Assertions.Assert;
+
+
+namespace  Ling.Tests.PlayMode.Plugin.UniRx.UniRxUniTaskBook
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Sample2_1
+    {
+		#region 定数, class, enum
+
+		public class PrintLogObserver<T> : IObserver<T>
+		{
+			public void OnCompleted()
+			{
+				UnityEngine.Debug.Log("OnCompleted");
+			}
+
+			public void OnError(Exception error)
+			{
+				UnityEngine.Debug.LogError(error);
+			}
+
+			public void OnNext(T value)
+			{
+				UnityEngine.Debug.Log(value);
+			}
+		}
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		public IObservable<int> CountDownObservable => _subject;
+
+		#endregion
+
+
+		#region private 変数
+
+		private Subject<int> _subject;
+		
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		[UnityTest]
+		public IEnumerator CountDownEeventProviderEvent()
+		{
+			var printLogObserver = new PrintLogObserver<int>();
+			_subject = new Subject<int>();
+
+			CountDownObservable
+				.Subscribe(printLogObserver);
+
+			int count = 10;
+			while (count > 0)
+			{
+				_subject.OnNext(count--);
+
+				yield return new WaitForEndOfFrame();
+			}
+
+			_subject.OnNext(0);
+			_subject.OnCompleted();
+		}
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
