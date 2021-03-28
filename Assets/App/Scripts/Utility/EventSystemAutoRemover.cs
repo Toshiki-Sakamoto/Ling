732a45cc3ef19115f7d8c584f5e78623ed4f215d
Toshizabeth <dearprograms2015@gmail.com>
2021-03-28 17:15:41 +0900

    EventSystemAutoRemover に名前を変更し、自分以外のEventSystemを制御するようにした

---
+++ Assets/App/Scripts/Utility/EventSystemAutoRemover.cs
@@ -0,0 +1,90 @@
+﻿// 
+// EventSystemRemover.cs  
+// ProductName Ling
+//  
+// Created by toshiki sakamoto on 2021.03.22
+// 
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using UnityEngine.EventSystems;
+
+namespace Ling.Utility
+{
+	/// <summary>
+	/// 起動時にEventSystemを一つにする。このスクリプトがついているシーン以外を破棄する
+	/// 新しく読み込まれたシーンのなかにEventSystemがあれば無効化する
+	/// </summary>
+	public class EventSystemRemover : MonoBehaviour 
+    {
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		private Scene _ownerScene = default;
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		private void RemoveEventSystemByScene(Scene scene)
+		{
+			// 自分のシーンは処理しない
+			if (_ownerScene == scene) return;
+
+			var eventSystemObjects = scene.GetRootGameObjects().Where(gameObject => gameObject.GetComponent<EventSystem>());
+			foreach (var eventSystem in eventSystemObjects)
+			{
+				eventSystem.SetActive(false);
+			}
+		}
+
+		#endregion
+
+
+		#region MonoBegaviour
+
+		/// <summary>
+		/// 初期処理
+		/// </summary>
+		void Awake()
+		{
+			_ownerScene = gameObject.scene;
+
+			// 現在のシーンすべてを走査する
+			for (int i = 0; i < SceneManager.sceneCount; ++i)
+			{
+				var scene = SceneManager.GetSceneAt(i);
+				RemoveEventSystemByScene(scene);
+			}
+
+			// シーン読み込み時のトリガー
+			SceneManager.sceneLoaded += (scene_, loadMode_) => 
+				{
+					RemoveEventSystemByScene(scene_);
+				};
+		}
+
+		#endregion
+	}
+}
