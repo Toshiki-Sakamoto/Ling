4e40b25651df06f21cf37eeff7f47cd99ad86561
Toshizabeth <dearprograms2015@gmail.com>
2021-04-14 17:49:23 +0900

    SortOrderSettingsをEditor機能を使わずに取得するように

---
+++ Assets/App/Scripts/Utility/Settings/UtilitySettings.cs
@@ -0,0 +1,62 @@
+﻿//
+// UtilitySettings.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.04.14
+//
+
+using UnityEngine;
+
+namespace Ling.Utility.Settings
+{
+	/// <summary>
+	/// Utilityフォルダ内の設定情報を管理する
+	/// </summary>
+	public class UtilitySettings : ScriptableObject
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+#if UNITY_EDITOR
+		public static UtilitySettings Load()
+		{
+			return Editor.AssetHelper.LoadAsset<UtilitySettings>();
+		}
+		public static UtilitySettings LoadCache()
+		{
+			return Editor.AssetHelper.LoadAsset<UtilitySettings>();
+		}
+#endif
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
