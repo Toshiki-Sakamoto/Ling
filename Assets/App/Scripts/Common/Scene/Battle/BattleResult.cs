e99e47e786aae10e1fa3c5280587dbd2fc5b5ab7
Toshizabeth <dearprograms2015@gmail.com>
2021-04-19 15:39:11 +0900

    Battle用のSceneResultクラス作成

---
+++ Assets/App/Scripts/Common/Scene/Battle/BattleResult.cs
@@ -0,0 +1,49 @@
+﻿//
+// BattleResult.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.04.19
+//
+
+namespace Ling.Common.Scene.Battle
+{
+	/// <summary>
+	/// BattleScene Result
+	/// </summary>
+	public class BattleResult : Scene.SceneResult
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
