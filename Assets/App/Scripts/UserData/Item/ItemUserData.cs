ee89d6f5634102c463f1ccabe202d5f4552cf3b2
Toshizabeth <dearprograms2015@gmail.com>
2021-04-22 17:16:27 +0900

    ItemUserDataとItemUserDataRepositoryの作成

---
+++ Assets/App/Scripts/UserData/Item/ItemUserData.cs
@@ -0,0 +1,49 @@
+﻿//
+// ItemUserData.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.04.22
+//
+
+namespace Ling.UserData.Item
+{
+	/// <summary>
+	/// ユーザーが持っているアイテムデータ
+	/// </summary>
+	public class ItemUserData : Utility.UserData.UserDataBase
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
---
+++ Assets/App/Scripts/UserData/Repository/ItemUserDataRepository.cs
@@ -0,0 +1,52 @@
+﻿//
+// ItemRepository.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.04.22
+//
+
+using Ling.UserData.Item;
+
+namespace Ling.UserData.Repository
+{
+	/// <summary>
+	/// プレイヤーが持っている持ち物
+	/// </summary>
+	public class ItemUserDataRepository: Utility.UserData.UserDataRepository<ItemUserData>
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
