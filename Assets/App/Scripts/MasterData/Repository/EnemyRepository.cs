cd874d4a7b9482c580e0194c6e46154e339e830f
Toshizabeth <dearprograms2015@gmail.com>
2021-04-22 17:16:39 +0900

    不要なusingを削除

--- Assets/App/Scripts/MasterData/Repository/EnemyRepository.cs
+++ Assets/App/Scripts/MasterData/Repository/EnemyRepository.cs
@@ -6,13 +6,6 @@
 //
 
 using Ling.MasterData.Chara;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
-using UnityEngine.UI;
 
 using Zenject;
 
