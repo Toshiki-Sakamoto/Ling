e9185b7e9274fc0a7404cbabba631348b93b990f
Toshizabeth <dearprograms2015@gmail.com>
2021-04-28 19:19:35 +0900

    キャラクタにありがちな現在値と最大値を持つValueObjectを作成

---
+++ Assets/App/Scripts/Map/Chara/CharaStatusValueObject.cs
@@ -0,0 +1,104 @@
+﻿//
+// CharaStatusValueObject.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.04.28
+//
+
+
+using System.Linq;
+using UnityEngine;
+using UniRx;
+
+namespace Ling.Chara
+{
+	/// <summary>
+	/// HPやスタミナなど、なにか足し合わせたり引いたりするステイタスを操作するValueObject
+	/// </summary>
+	[System.Serializable]
+	public class CharaStatusValueObject
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		[SerializeField] private LongReactiveProperty _current = default;
+		[SerializeField] private LongReactiveProperty _max = default;
+
+		#endregion
+
+
+		#region プロパティ
+
+		public LongReactiveProperty Current => _current;
+		public LongReactiveProperty Max => _max;
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		public CharaStatusValueObject(long value, long maxValue)
+		{
+			_current = new LongReactiveProperty(value);
+			_max = new LongReactiveProperty(maxValue);
+		}
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		/// <summary>
+		/// 値を設定
+		/// </summary>
+		public void SetCurrent(long value) =>
+			_current.SetValueAndForceNotify(value);
+
+		/// <summary>
+		/// 現在値に足し合わせる
+		/// </summary>
+		public void AddCurrent(long value) =>
+			SetCurrent(_current.Value + value);
+			
+		/// <summary>
+		/// 現在値から引く
+		/// </summary>
+		public void SubCurrent(long value) =>
+			SetCurrent(_current.Value - value);
+
+
+		/// <summary>
+		/// 値を設定
+		/// </summary>
+		public void SetMax(long value) =>
+			_max.SetValueAndForceNotify(value);
+
+		/// <summary>
+		/// 現在値に足し合わせる
+		/// </summary>
+		public void AddMax(long value) =>
+			SetMax(_max.Value + value);
+			
+		/// <summary>
+		/// 現在値から引く
+		/// </summary>
+		public void SubMax(long value) =>
+			SetMax(_max.Value - value);
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
