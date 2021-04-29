4d5a3364c08f49bcd0cbca62ce7b2ed45acbf7bb
Toshizabeth <dearprograms2015@gmail.com>
2021-04-29 16:41:03 +0900

    LvTableMaster

---
+++ Assets/App/Scripts/Common/MasterData/Chara/LvTableMaster.cs
@@ -0,0 +1,74 @@
+﻿//
+// LvTableMaster.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.04.29
+//
+
+using UnityEngine;
+using Utility.MasterData;
+using Utility.Attribute;
+using System.Collections.Generic;
+
+namespace Ling.MasterData.Chara
+{
+	/// <summary>
+	/// レベルテーブル
+	/// </summary>
+	[CreateAssetMenu(menuName = "MasterData/LvTable", fileName = "LvTableMaster")]
+	public class LvTableMaster : MasterDataBase
+	{
+		#region 定数, class, enum
+
+		[System.Serializable]
+		public class Data
+		{
+			[SerializeField, FieldName("Lv")]
+			private int _lv = 0;
+
+			[SerializeField, FieldName("経験値")]
+			private int _exp = 0;
+
+			[SerializeField, FieldName("ちから")]
+			private int _power = 0;
+		}
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		[SerializeField, FieldName("キャラクタID")]
+		private int _id = default;
+
+		[SerializeField, FieldName("テーブル")]
+		private List<Data> _data = default;
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
