7cf2cdbbeb5c3056274b5f7241c3683b8f271f01
Toshizabeth <dearprograms2015@gmail.com>
2021-05-10 22:26:11 +0900

    落とし物をマップに配置するクラスを作成

---
+++ Assets/App/Scripts/Map/DropItemBuilder.cs
@@ -0,0 +1,49 @@
+﻿//
+// DropItemBuilder.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.05.10
+//
+
+namespace Ling.Map
+{
+	/// <summary>
+	/// 落とし物をマップに配置する
+	/// </summary>
+	public class DropItemBuilder
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
