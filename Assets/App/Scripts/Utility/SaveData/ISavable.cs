acf0f28a55f7e0a29c89af3bbc4572110d735bbd
Toshizabeth <dearprograms2015@gmail.com>
2021-05-20 10:20:23 +0900

    セーブデータ周りのヘルパクラスの作成

---
+++ Assets/App/Scripts/Utility/SaveData/ISavable.cs
@@ -0,0 +1,17 @@
+﻿//
+// ISavable.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.05.19
+//
+
+namespace Utility.SaveData
+{
+	/// <summary>
+	/// セーブ可能
+	/// </summary>
+	public class ISavable
+	{
+		//void Save<T>(string key);
+	}
+}
