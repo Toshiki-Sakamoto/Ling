a7f1d037c5cfe5ceaa385e22d5c151dc49ccb68c
Toshizabeth <dearprograms2015@gmail.com>
2021-05-29 17:26:43 +0900

    Serialize可能なValueObjectクラス作成

---
+++ Assets/App/Scripts/Utility/ValueObject.cs
@@ -0,0 +1,65 @@
+﻿//
+// ValueObject.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.05.29
+//
+
+using UnityEngine;
+
+namespace Utility
+{
+	/// <summary>
+	/// 値をラップする
+	/// </summary>
+	[System.Serializable]
+	public class ValueObject<T>
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		[SerializeField] private T _value = default;
+
+		#endregion
+
+
+		#region プロパティ
+
+		public T Value => _value;
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		public ValueObject()
+		{
+		}
+
+		public ValueObject(T value)
+		{
+			_value = value;
+		}
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
