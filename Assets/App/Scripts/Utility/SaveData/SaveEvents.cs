1ca0f2b4311d77299397b8cf930a8096d710bcf2
Toshizabeth <dearprograms2015@gmail.com>
2021-05-29 15:34:32 +0900

    セーブイベントの追加

---
+++ Assets/App/Scripts/Utility/SaveData/SaveEvents.cs
@@ -0,0 +1,16 @@
+﻿//
+// SaveEvents.cs
+// ProductName Ling
+//
+// Created by toshiki sakamoto on 2021.05.28
+//
+
+namespace Utility.SaveData
+{
+    /// <summary>
+    /// セーブ実行通知
+    /// </summary>
+    public class EventSaveAll
+    {
+    }
+}
