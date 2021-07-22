d18620a5cb16f664328e40f1eb3d1c9304af58cb
Toshizabeth <dearprograms2015@gmail.com>
2021-07-23 01:33:07 +0900

    Test

---
+++ Assets/App/Scripts/Common/CommonEvents.cs
@@ -0,0 +1,17 @@
+﻿//
+// CommonEvents.cs
+// ProductName Ling
+//
+// Created by  on 2021.06.28
+//
+
+namespace Ling.Common
+{
+	/// <summary>
+	/// ログ通知
+	/// </summary>
+	public class NoticeEvent
+	{
+		public string Message;
+	}
+}
