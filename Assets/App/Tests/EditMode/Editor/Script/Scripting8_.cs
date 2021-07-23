e84e7b8ee7ebc88ecefe8ca0245df46960d6e5a5
Toshizabeth <dearprograms2015@gmail.com>
2021-07-23 19:57:04 +0900

    C#8の機能をテストするスクリプト作成

---
+++ Assets/App/Tests/EditMode/Editor/Script/Scripting8_.cs
@@ -0,0 +1,70 @@
+﻿//
+// Scripting8_.cs
+// ProductName Ling
+//
+// Created by  on 2021.07.23
+//
+
+using NUnit.Framework;
+using System.Collections.Generic;
+using Assert = UnityEngine.Assertions.Assert;
+
+
+namespace Ling.Tests.EditMode.Script
+{
+	/// <summary>
+	/// C#8
+	/// </summary>
+	public class Scripting8_
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public, protected 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region コンストラクタ, デストラクタ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Null合体割当演算子()
+		{
+			// 左オペランドがNullだった場合のみ右の値を左に代入する
+			var str = default(string);
+
+			str ??= "Test";
+
+			Assert.AreEqual("Test", str, "文字列が一致する");
+		}
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+	}
+}
