6c684f39144118c3558a3f30163444aed55e2d07
Toshizabeth <dearprograms2015@gmail.com>
2021-08-08 22:38:39 +0900

    Subject、ReactivePropertyのテスト

--- Assets/App/Tests/EditMode/Editor/Plugin/UniRxTest/ObserverTest.cs
+++ Assets/App/Tests/EditMode/Editor/Plugin/UniRxTest/ObserverTest.cs
@@ -7,6 +7,8 @@
 
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using UniRx;
 
 using Assert = UnityEngine.Assertions.Assert;
 
@@ -30,8 +32,8 @@ namespace Ling.Tests.Plugin.UniRxTest
 		}
 
 		/// <summary>
-        /// イベントを受け取る方
-        /// </summary>
+		/// イベントを受け取る方
+		/// </summary>
 		public sealed class TestObserver : IObserver<int>
 		{
 			public bool IsCompleted { get; private set; }
@@ -50,6 +52,181 @@ namespace Ling.Tests.Plugin.UniRxTest
 			{
 				Value = value;
 			}
+		}
+
+		/// <summary>
+		/// SubjectはIObserver(受信）とIObservable（送信）の両方を受け持つ
+		/// </summary>
+		public sealed class MySubject<T> : ISubject<T>, IDisposable
+		{
+			/// <summary>
+			/// 購読状態を管理する
+			/// Disposeを呼ぶとSubjetからObserverを削除する
+			/// </summary>
+			private sealed class Subscription : IDisposable
+			{
+				private readonly IObserver<T> _observer;
+				private MySubject<T> _parent;
+				private readonly object _gate = new object();
+
+				public Subscription(IObserver<T> observer, MySubject<T> parent)
+				{
+					_observer = observer;
+					_parent = parent;
+				}
+
+				public void Dispose()
+				{
+					lock (_gate)
+					{
+						if (_parent == null) return;
+						lock (_parent._gate)
+						{
+							_parent._observers?.Remove(_observer);
+							_parent = null;
+						}
+					}
+				}
+			}
+
+			private readonly object _gate = new object();   // 排他処理用
+			private List<IObserver<T>> _observers;          // 登録されたObserver一覧
+			private bool _isDisposed;   // 破棄されたか
+			private bool _isStopped;    // 停止状態か
+			private Exception _error;
+
+			public void OnNext(T value)
+			{
+				lock (_gate)
+				{
+					ThrowIfDisposed();
+
+					if (_isStopped) return;
+					if (_observers != null)
+					{
+						foreach (var observer in _observers)
+						{
+							// 各種Observerにイベントを伝える
+							observer.OnNext(value);
+						}
+					}
+				}
+			}
+
+			public void OnError(Exception error)
+			{
+				lock (_gate)
+				{
+					ThrowIfDisposed();
+					if (_isStopped) return;
+
+					_error = error;
+
+					if (_observers != null)
+					{
+						foreach (var observer in _observers)
+						{
+							observer.OnError(error);
+						}
+
+						_observers.Clear();
+						_observers = null;
+					}
+
+					// 停止
+					_isDisposed = true;
+				}
+			}
+
+			public void OnCompleted()
+			{
+				lock (_gate)
+				{
+					ThrowIfDisposed();
+
+					if (_isStopped) return;
+					if (_observers != null)
+					{
+						foreach (var observer in _observers)
+						{
+							observer.OnCompleted();
+						}
+
+						_observers.Clear(); // Completeしたら中身を消す
+						_observers = null;
+					}
+
+					_isStopped = true;
+				}
+			}
+
+
+			// 登録する部分
+			public IDisposable Subscribe(IObserver<T> observer)
+			{
+				if (observer == null) throw new ArgumentException();
+
+				lock (_gate)
+				{
+					ThrowIfDisposed();
+
+					if (!_isStopped)
+					{
+						// Observerのリストに登録
+						if (_observers == null) _observers = new List<IObserver<T>>();
+						_observers.Add(observer);
+
+						return new Subscription(observer, this);
+					}
+					else
+					{
+						// 終了してるのを伝える
+						if (_error != null)
+						{
+							observer.OnError(_error);
+						}
+						else
+						{
+							observer.OnCompleted();
+						}
+
+						return Disposable.Empty;
+					}
+				}
+			}
+
+			private void ThrowIfDisposed()
+			{
+				lock (_gate)
+				{
+					if (_isDisposed) throw new ObjectDisposedException(nameof(GetType));
+				}
+			}
+
+			public void Dispose()
+			{
+				lock (_gate)
+				{
+					if (_isDisposed) return;
+
+					_isDisposed = true;
+					_observers = null;
+				}
+			}
+		}
+
+		public class SampleObserver : IObserver<int>
+		{
+			public int Value { get; private set; }
+
+			public void OnNext(int value)
+			{
+				Value = value;
+			}
+
+			public void OnError(Exception e) { }
+
+			public void OnCompleted() { }
 		}
 
 		#endregion
@@ -87,6 +264,79 @@ namespace Ling.Tests.Plugin.UniRxTest
 
 			Assert.IsTrue(observer.IsCompleted);
 			Assert.AreEqual(1, observer.Value);
+		}
+
+		[Test]
+		public void IObservableテストコード()
+		{
+			// IObservableはIObserverを登録できる。という振る舞いをするインターフェース
+			// メッセージ送信側で使う
+
+			// SubjectはObserverを登録/かんりしイベントの発行を実行できる
+		}
+
+		[Test]
+		public void 自作Subjectテスト()
+        {
+			var mySubject = new MySubject<int>();
+			var sampleObserver = new SampleObserver();
+
+			var disposable = mySubject.Subscribe(sampleObserver);
+
+			mySubject.OnNext(1);
+
+			Assert.AreEqual(1, sampleObserver.Value, "受け取れてること");
+
+			// 破棄
+			disposable.Dispose();
+
+			mySubject.OnNext(2);
+
+			Assert.AreEqual(1, sampleObserver.Value, "破棄しているので更新されない");
+
+			mySubject.OnCompleted();
+			mySubject.Dispose();
+        }
+
+		[Test]
+		public void SubjectにIObserverの代わりにラムダを登録する()
+		{
+			var value = default(string);
+
+			var subject = new Subject<string>();
+			subject.Subscribe(
+				onNext: x => value = x,
+				onError: error => value = "Error",
+				onCompleted: () => value = "Complete");
+
+			subject.OnNext("Next");
+			Assert.AreEqual("Next", value);
+
+			subject.OnCompleted();
+			Assert.AreEqual("Complete", value);
+		}
+
+		[Test]
+		public void ReactivePropertyテスト()
+		{
+			int value = 0;
+
+			// Subjectの機能が同梱された変数として振る舞う
+			var rp = new ReactiveProperty<int>(0);
+			rp.Subscribe(x => value = x);
+
+			rp.Value = 1;
+
+			Assert.AreEqual(1, value);
+		}
+
+		[Test]
+		public void Operatorテスト()
+		{
+			// SubjectとObserverの中間に挟み込むことが出来るオブジェクト
+			// SubjectからObserverに伝達されるメッセージの加工や発行タイミングの調整が可能
+
+
 		}
 
 
