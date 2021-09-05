6579c3a5d39cb72cd679dc3d7c89f7d86dbbc139
Toshizabeth <dearprograms2015@gmail.com>
2021-09-06 00:13:08 +0900

    エフェクト定数追加

---
+++ Assets/App/Scripts/Const/EffectType.cs
@@ -0,0 +1,20 @@
+﻿//
+// EffectType.cs
+// ProductName Ling
+//
+// Created by  on 2021.09.02
+//
+
+namespace Ling.Const
+{
+	/// <summary>
+	/// エフェクトの種類
+	/// </summary>
+	public enum EffectType
+	{
+		None,
+
+		Damage,
+		Heal,
+	}
+}
