36166cabcc2390de35e28ce92a2d452d9519a0f0
Toshizabeth <dearprograms2015@gmail.com>
2021-09-14 20:12:37 +0900

    ミニマップ用のプール管理者を作成

---
+++ Assets/App/Scripts/Map/MiniMapPoolManager.cs
@@ -0,0 +1,51 @@
+﻿// 
+// MiniMapPoolManager.cs  
+// ProductName Ling
+//  
+// Created by  on 2021.09.13
+// 
+
+using UnityEngine;
+
+namespace Ling.Map
+{
+	/// <summary>
+	/// ミニマップ上のプール管理者
+	/// </summary>
+	public class MiniMapPoolManager : Utility.Pool.PoolManager<Const.TileFlag, MiniMapPoolManager> 
+	{
+		#region 定数, class, enum
+
+		#endregion
+
+
+		#region public 変数
+
+		#endregion
+
+
+		#region private 変数
+
+		#endregion
+
+
+		#region プロパティ
+
+		#endregion
+
+
+		#region public, protected 関数
+
+		#endregion
+
+
+		#region private 関数
+
+		#endregion
+
+
+		#region MonoBegaviour
+
+		#endregion
+	}
+}
